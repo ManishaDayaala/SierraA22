029efba84799715b13009d13d52b14358edc6164
ManishaDayaala <Manishadayaala@gmail.com>
2025-04-14 17:07:58 +0530

    Update SierraA22.py

--- SierraA22.py
+++ SierraA22.py
@@ -26,7 +26,7 @@ def set_random_seed(seed_value=42):
     tf.random.set_seed(seed_value)
 
 # Define the main folder path
-MAINFOLDER = r"D:\APPdata_SierraA22"
+MAINFOLDER = r"./APPdata_SierraA22"
 
 # Create other paths relative to the main folder
 training_file_path = os.path.join(MAINFOLDER, "Training", "Training_SierraA22.xlsx")  # FIXED TRAINING DATA
